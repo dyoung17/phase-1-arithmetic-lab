add7c0f2b278cdbf429672139fccb6edbcac5ae9
Daven Young <davenyoung@msn.com>
2022-04-04 19:02:16 -0600

    commit tree e0c624f0bea1da566702514f00a9832d4786c407

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2;
 
-// Write your code here
+let number = 0
+const random = number + 1;
+
+let num3 = 24
+let num4 = 20
+const mod = num3 % num4;
+
+
+let max = (1,2,3,20)
+max.max(1,2,3,20);
